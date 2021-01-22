194c0ced2d12bd94ead5e650b5d389463b7455da
nour963 <nouraslaoui@gmail.com>
2021-01-22 19:12:19 +0100

    commit

--- nginx-pythonFront-pythonBack/py_backend/app.py
+++ nginx-pythonFront-pythonBack/py_backend/app.py
@@ -1,12 +1,24 @@
 from random import randint
 from time import sleep
-
 from flask import request
 from flask import Flask
+from jaeger_client import Config
+from flask_opentracing import FlaskTracing
+
 app = Flask(__name__)
+#tracing
+config = Config(
+    config={
+        'sampler':
+        {'type': 'const',
+         'param': 1},
+                        'logging': True,
+                        'reporter_batch_size': 1,}, 
+                        service_name="service")
+jaeger_tracer = config.initialize_tracer()
+tracing = FlaskTracing(jaeger_tracer, True, app)
 
 counter_value = 1
-
 def get_counter():
     return str(counter_value)
 
--- nginx-pythonFront-pythonBack/py_frontend/page1/app.py
+++ nginx-pythonFront-pythonBack/py_frontend/page1/app.py
@@ -1,7 +1,22 @@
 import os
 import requests
 from flask import Flask, render_template
+from jaeger_client import Config
+from flask_opentracing import FlaskTracing
+
 app = Flask(__name__)
+#tracing
+config = Config(
+    config={
+        'sampler':
+        {'type': 'const',
+         'param': 1},
+                        'logging': True,
+                        'reporter_batch_size': 1,}, 
+                        service_name="service")
+jaeger_tracer = config.initialize_tracer()
+tracing = FlaskTracing(jaeger_tracer, True, app)
+
 def get_counter(counter_endpoint):
     counter_response = requests.get(counter_endpoint)
     return counter_response.text
